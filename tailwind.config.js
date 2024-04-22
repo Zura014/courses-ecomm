2df52382807bdaea5d968fd3de1979a16ee7c1fd
Zura014 <zuragagnidze50@gmail.com>
2024-04-22 23:37:54 +0400

    yohohohooooo

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    
+    colors: {
+      primary: "#F14D5D",
+      secondary: "#015ABD",
+      heading: "#14133b",
+      text: "#77838F",
+      white: "#fff",
+      borderC: "#ddd"
+    },
+    screens: {
+      xs: "0",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+      xxl: "1400px",
+    },
+    extend: {
+      fontFamily: {
+        primaryFont: ["GilRoy", "sans-serif"],
+        secondaryFont: ["Jost", "sans-serif"],
+      },
+    },
+  },
+  plugins: [ 
+  ],
+}
