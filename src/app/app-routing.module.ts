a7daf5e5e5fa64985f55aa157badb97f7485fdd9
Zura156 <117541032+Zura014@users.noreply.github.com>
2024-06-25 16:09:00 +0400

    ff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { CoursesComponent } from './pages/courses/courses.component';
-
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+    title: 'Edumel - Online Courses Website',
+  },
+  { path: 'home', component: HomeComponent, title: 'Edumel - Home Page' },
+  { path: 'about', component: AboutComponent, title: 'Edumel - About Page' },
   {
     path: 'courses',
     loadChildren: () =>
       import('./pages/courses/courses.module').then((m) => m.CoursesModule),
+    title: 'Edumel - Courses Page',
   },
   {
     path: 'auth',
     loadChildren: () =>
       import('./pages/auth/auth.module').then((m) => m.AuthModule),
+    title: 'Edumel - Authorization Page',
   },
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
+@NgModule({ imports: [RouterModule.forRoot(routes)], exports: [RouterModule] })
 export class AppRoutingModule {}
